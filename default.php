8e2fa9a0620dd0f5dfa1bcaf9da94eebb270565d
Evgeny Svirsky <e.svirsky@tut.by>
2013-04-12 07:56:25 +0300

    initial commit

---
+++ default.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Lang" content="en">
+<meta name="author" content="">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<title>User's Salary list</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+    <body>
+        <div class="main-width">
+            <h1>User's Salary list</h1>  
+            <div class="content">
+                <label for="use_ajax">Use AJAX</label>
+                <input type="checkbox" name="use_ajax" id="use_ajax" />
+                <div class="table-wrapper">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>First Name</td>
+                                <th>Surname</td>
+                                <th>Date of Birth</td>
+                                <th>Salary</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>Surname</td>
+                                <td>Date of Birth</td>
+                                <td>Salary</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
